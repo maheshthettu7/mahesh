e8cc4ef6bdbf94d4cba9d90956f08899ba421de2
maheshthettu7 <144412209+maheshthettu7@users.noreply.github.com>
2024-01-24 14:57:46 +0530

    Update and rename eda.py to ml_training.py

--- eda.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-%matplotlib inline
-import warnings
-warnings.filterwarnings('ignore')
-import seaborn as sns
---
+++ ml_training.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.model_selection import RandomizedSearchCV
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+import warnings
+df = pd.read_csv("data/StudentsPerformance.csv")
+
