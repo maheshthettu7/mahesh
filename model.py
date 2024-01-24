cc9859408683f2fd96e60fd0dcf1d30e1134fa63
maheshthettu7 <144412209+maheshthettu7@users.noreply.github.com>
2024-01-24 15:25:49 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,97 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.model_selection import RandomizedSearchCV
+from xgboost import XGBRegressor
+import warnings
+df = pd.read_csv("StudentsPerformance.csv")
+X = df.drop(columns=['math_score'], axis=1)
+y = df['math_score']
+num_features = X.select_dtypes(exclude="object").columns # all numeric features
+cat_features = X.select_dtypes(include="object").columns # all categorical features
 
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+
+numeric_transformer = StandardScaler()
+oh_transformer = OneHotEncoder()
+
+#creating a transform pipeline
+preprocessor = ColumnTransformer(
+    [
+        ("OneHotEncoder", oh_transformer, cat_features),
+         ("StandardScaler", numeric_transformer, num_features),        
+    ]
+)
+X = preprocessor.fit_transform(X)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+# creating an evaluate function to check out model
+def evaluate_model(true, predicted):
+    mae = mean_absolute_error(true, predicted)
+    mse = mean_squared_error(true, predicted)
+    rmse = np.sqrt(mean_squared_error(true, predicted))
+    r2_square = r2_score(true, predicted)
+    return mae, rmse, r2_square
+# Create a compositon of various models to use
+models = {
+    "Linear Regression": LinearRegression(),
+    "Lasso": Lasso(),
+    "Ridge": Ridge(),
+    "K-Neighbors Regressor": KNeighborsRegressor(),
+    "Decision Tree": DecisionTreeRegressor(),
+    "Random Forest Regressor": RandomForestRegressor(),
+    "XGBRegressor": XGBRegressor(), 
+    "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+    "AdaBoost Regressor": AdaBoostRegressor()
+}
+
+# iterate through all the models and then predict values based on it
+model_list = []
+r2_list =[]
+
+for i in range(len(list(models))):
+    model = list(models.values())[i]
+    model.fit(X_train, y_train) # Train model
+    pickle.dump(model, open(f'{list(models.values())[i]}.pkl', 'wb'))
+
+    # Make predictions
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+    
+    # Evaluate Train and Test dataset
+    model_train_mae , model_train_rmse, model_train_r2 = evaluate_model(y_train, y_train_pred)
+
+    model_test_mae , model_test_rmse, model_test_r2 = evaluate_model(y_test, y_test_pred)
+
+    
+    print(list(models.keys())[i])
+    model_list.append(list(models.keys())[i])
+    
+    print('Model performance for Training set')
+    print("- Root Mean Squared Error: {:.4f}".format(model_train_rmse))
+    print("- Mean Absolute Error: {:.4f}".format(model_train_mae))
+    print("- R2 Score: {:.4f}".format(model_train_r2))
+
+    print('----------------------------------')
+    
+    print('Model performance for Test set')
+    print("- Root Mean Squared Error: {:.4f}".format(model_test_rmse))
+    print("- Mean Absolute Error: {:.4f}".format(model_test_mae))
+    print("- R2 Score: {:.4f}".format(model_test_r2))
+    r2_list.append(model_test_r2)
+    
+    print('='*35)
+    print('\n')
+
+print(pd.DataFrame(list(zip(model_list, r2_list)), columns=['Model Name', 'R2_Score']).sort_values(by=["R2_Score"],ascending=False))
+
+  
