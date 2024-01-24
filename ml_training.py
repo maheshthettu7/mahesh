f9a10ae8ce612d206bf479ae661107064ae843bb
maheshthettu7 <144412209+maheshthettu7@users.noreply.github.com>
2024-01-24 15:10:05 +0530

    Update ml_training.py

--- ml_training.py
+++ ml_training.py
@@ -1,18 +1,19 @@
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import streamlit
-# from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-# from sklearn.neighbors import KNeighborsRegressor
-# from sklearn.tree import DecisionTreeRegressor
-# from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
-# from sklearn.svm import SVR
-# from sklearn.linear_model import LinearRegression, Ridge, Lasso
-# from sklearn.model_selection import RandomizedSearchCV
-# from xgboost import XGBRegressor
-# import warnings
-# df = pd.read_csv("StudentsPerformance.csv")
-# streamlit.dataframe(df.head())
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.model_selection import RandomizedSearchCV
+from xgboost import XGBRegressor
+import warnings
+streamlit.title("Sample data for model")
+df = pd.read_csv("StudentsPerformance.csv")
+streamlit.dataframe(df.head())
 streamlit.title("input data for ml model")
 
